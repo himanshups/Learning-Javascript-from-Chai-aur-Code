5716c2cbddeb4a0cd09d6f66e1dc022948d009ad
himanshups <74396707+himanshups@users.noreply.github.com>
2024-05-12 22:54:57 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,42 @@
+"use strict"; // used to indicate new version of JS code
+
+// alert(4+4) -  incorrect to write in Node.js , acceptable in browser
+
+console.log(3
+    +
+    3) // code readiblity shoudl be high
+
+console.log("Jake") 
+
+// some standard documentation - mdn and tc39.es
+
+/* Datatypes */
+
+let name = "Rohit daniel"
+let emailID = "rohit@dummy"
+let state=true;
+
+let State;
+
+/*
+Primitives data types -
+
+number => Range is 2 to power of 53 
+bigint => 
+string => "" or ''
+boolean => two values only > true/false
+null => standalone value
+undefined =>
+symbol => unique
+
+Derrived/Complex datatypes 
+
+object -
+
+*/
+
+console.log(typeof "Kunal")
+
+console.log(typeof null) // object 
+
+console.log(typeof undefined) /// undefined
